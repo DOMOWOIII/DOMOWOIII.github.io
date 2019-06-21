6a7acd3b541792e955e47ed2ae43ce0f5df08609
OWIII <songlab.ru@gmail.com>
2019-06-21 20:58:44 +0300

    +!

---
+++ 15.Kinopoisk-mini/js/script.js
@@ -0,0 +1,33 @@
+const searchForm = document.querySelector("#search-form");
+const movie = document.querySelector("#movies");
+const urlPoster = "https://image.tmdb.org/t/p/w500";
+
+function apiSearch(event) {
+    event.preventDefault();
+    const searchText = document.querySelector(".form-control").value;
+    const server = "https://api.themoviedb.org/3/search/multi?api_key=ad2b38c3623133c5e9dd093465546d14&language=ru&query=" + searchText;
+    movie.innerHTML = '<div class="spinner"></div>';
+
+    fetch(server)
+        .then(function(value) {
+            return value.json();
+        })
+        .then(function(output) {
+            let inner = "";
+            output.results.forEach(function(item) {
+                let nameItem = item.name || item.title;
+                inner += `
+            <div class = 'col-sm-6 item'>
+            <img src= "${urlPoster + item.poster_path}" alt = "${nameItem}">
+            <h5>${nameItem}</h5>
+            </div>
+            `;
+            });
+            movie.innerHTML = inner;
+        })
+        .catch(function(reason) {
+            movie.innerHTML = "Упс, что-то пошло не так";
+            console.log("error:" + reason.status);
+        });
+};
+searchForm.addEventListener("submit", apiSearch);
